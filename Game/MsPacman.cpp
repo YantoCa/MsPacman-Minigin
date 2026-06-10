b7320ef9e428cefb3b96f9c00649ed8e3e34c982
Yanto Caluwé <yantocaluwe1@gmail.com>
2026-06-10 23:39:05 +0200

    Test MsPacman.cpp cleanup

--- Game/MsPacman.cpp
+++ Game/MsPacman.cpp
@@ -25,84 +25,66 @@ void MsPacman::Initialize() {
 	auto font = dae::ResourceManager::GetInstance().LoadFont("Lingua.otf", 36);
 
 	// Background
-	auto go = std::make_unique<dae::GameObject>();
-	go->AddComponent<dae::RenderComponent>("background.png");
-	scene.Add(std::move(go));
+		auto bg = std::make_unique<dae::GameObject>();
+		bg->AddComponent<dae::RenderComponent>("background.png");
+		scene.Add(std::move(bg));
 
-	// Keyboard Player
-	go = std::make_unique<dae::GameObject>();
-	go->AddComponent<dae::RenderComponent>("logo.png");
-	go->SetPosition(358, 180);
-	auto* pPointsCompP1 = go->AddComponent<dae::PointsComponent>();
-	go->AddComponent<dae::MovementComponent>();
+	// Player 1 (Keyboard dummy)
+		auto p1 = std::make_unique<dae::GameObject>();
+		p1->AddComponent<dae::RenderComponent>("logo.png");
+		p1->SetPosition(358, 180);
+		auto* pPointsCompP1 = p1->AddComponent<dae::PointsComponent>(); // Grab pointer to subject to attach it to observer
+		p1->AddComponent<dae::MovementComponent>();
 
-	// Bind bindings to play
-	input.AddBinding(std::make_unique<dae::KeyboardBinding>(SDL_SCANCODE_W, std::make_unique<dae::AddPointsCommand>(*go, 1), dae::KeyState::OnHold));
-	//input.AddBinding(std::make_unique<dae::KeyboardBinding>(SDL_SCANCODE_W, std::make_unique<dae::MoveCommand>(*go, glm::vec3{ 0, 1, 0 }), dae::KeyState::OnPress));
-	//input.AddBinding(std::make_unique<dae::KeyboardBinding>(SDL_SCANCODE_W, std::make_unique<dae::MoveCommand>(*go, glm::vec3{0, 0, 0}), dae::KeyState::OnRelease));
+		// Bind bindings to player 1
+		input.AddBinding(std::make_unique<dae::KeyboardBinding>(SDL_SCANCODE_W, std::make_unique<dae::AddPointsCommand>(*p1, 1), dae::KeyState::OnHold)); 
 
-	// Display points
-	auto DispalyPoints = std::make_unique<dae::GameObject>();
-	DispalyPoints->SetPosition(10.f, 325.f);
-	DispalyPoints->AddComponent<dae::TextComponent>("Score: ", font);
-	auto* pDisplayObs = DispalyPoints->AddComponent<dae::DisplayPointsComponent>();
+		// Observers Player 1
+			// Display points UI
+			auto DispalyPoints = std::make_unique<dae::GameObject>();
+				DispalyPoints->SetPosition(10.f, 325.f);
+				DispalyPoints->AddComponent<dae::TextComponent>("Score: 0", font);
+				auto* pDisplayObs = DispalyPoints->AddComponent<dae::DisplayPointsComponent>();
 
-	// Couple observer
-	pPointsCompP1->AddObserver(pDisplayObs);
+		// Couple Subjet and observer
+		pPointsCompP1->AddObserver(pDisplayObs);
 
-	scene.Add(std::move(DispalyPoints));
-	scene.Add(std::move(go));
+		// Empty out
+		scene.Add(std::move(DispalyPoints));
+		scene.Add(std::move(p1));
 
-	//go = std::make_unique<dae::GameObject>();
-	//go->AddComponent<dae::RenderComponent>("logo.png");
-	//go->SetPosition(358, 180);
-	//go->AddComponent<dae::PointsComponent>();
+	// Player 2 (Gamepad dummy)
+		auto p2 = std::make_unique<dae::GameObject>();
+		p2->AddComponent<dae::RenderComponent>("logo.png");
+		p2->SetPosition(358, 180);
+		auto* pPointsCompP2 = p2->AddComponent<dae::PointsComponent>(); // Grab pointer to subject to attach it to observer
+		p2->AddComponent<dae::MovementComponent>();
 
-	//// Bind bindings to play
-	//auto& controller0 = input.AddController(0);
-	//input.AddBinding(std::make_unique<dae::ControllerBinding>(controller0, dae::Gamepad::ControllerButton::GAMEPAD_A, std::make_unique<dae::AddPointsCommand>(*go, 1), dae::KeyState::OnPress));
+		// Bind bindings to player 2
+		auto& controller0 = input.AddController(0);
+		input.AddBinding(std::make_unique<dae::ControllerBinding>(controller0, dae::Gamepad::ControllerButton::GAMEPAD_A, std::make_unique<dae::AddPointsCommand>(*p2, 1), dae::KeyState::OnHold));
 
+		// Observers Player 2
+			// Display points UI
+				DispalyPoints = std::make_unique<dae::GameObject>();
+				DispalyPoints->SetPosition(10.f, 375.f);
+				DispalyPoints->AddComponent<dae::TextComponent>("Score: 0", font);
+				pDisplayObs = DispalyPoints->AddComponent<dae::DisplayPointsComponent>();
 
-	//// Display points
-	//DispalyPoints = std::make_unique<dae::GameObject>();
-	//DispalyPoints->SetPosition(10.f, 375.f);
-	//DispalyPoints->AddComponent<dae::TextComponent>("", font);
-	//DispalyPoints->AddComponent<dae::DisplayPointsComponent>();
-	//scene.Add(std::move(DispalyPoints));
+		// Couple Subject and observer
+		pPointsCompP2->AddObserver(pDisplayObs);
 
-	//scene.Add(std::move(go));
-
-	// Text
-	auto to = std::make_unique<dae::GameObject>();
-	to->AddComponent<dae::TextComponent>("Programming 4 Assignment", font);
-	//to->SetColor({ 255, 255, 0, 255 }); // TODO add function Setcolor
-	to->SetPosition(292, 20);
-	scene.Add(std::move(to));
-
+		// Empty out
+		scene.Add(std::move(DispalyPoints));
+		scene.Add(std::move(p2));
+ 
 	// FPS counter
-	to = std::make_unique<dae::GameObject>();
-	to->AddComponent<dae::FPSComponent>(font);
-	scene.Add(std::move(to));
-
-	//// Rotating objects
-	//auto rotating = std::make_unique<dae::GameObject>();
-	//rotating->AddComponent<dae::RenderComponent>("logo.png");
-	//rotating->SetPosition(200, 300);
-	//rotating->AddComponent<dae::RotationComponent>(270.0f, false);
-
-	//auto rotating2 = std::make_unique<dae::GameObject>();
-	//rotating2->AddComponent<dae::RenderComponent>("logo.png");
-	//rotating2->SetPosition(100, 0);
-	//rotating2->SetParent(rotating.get(), false);
-	//rotating2->AddComponent<dae::RotationComponent>(360.0f, true);
-	//
-	//scene.Add(std::move(rotating)); // if added to scene too early, you cant add childeren due to it becoming a nullptr after std::move()
-	//scene.Add(std::move(rotating2));
-
-
+		auto fps = std::make_unique<dae::GameObject>();
+		fps->AddComponent<dae::FPSComponent>(font);
+		scene.Add(std::move(fps));
 
 	// ImGui 
-	auto gui = std::make_unique<dae::GameObject>();
-	gui->AddComponent<dae::ImGuiComponent>();
-	scene.Add(std::move(gui));
+		auto gui = std::make_unique<dae::GameObject>();
+		gui->AddComponent<dae::ImGuiComponent>();
+		scene.Add(std::move(gui));
 }
